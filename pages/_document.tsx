9f643e1e6f1963ece50ab13b2d247bf0636b5f57
teltelbozz <nobuty717@gmail.com>
2024-10-19 18:17:20 +0900

    add Chatsimple code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <chat-bot platform_id="4d61f81f-3ca3-48a5-8dca-4d36d83b0e5a" user_id="8e2464b9-c444-4555-91a1-07967c44d907" chatbot_id="769f2c53-55d6-447c-824b-1da783b77098" ><a href="https://www.chatsimple.ai/?utm_source=widget&utm_medium=referral">chatsimple</a></chat-bot><script src="https://cdn.chatsimple.ai/chat-bot-loader.js" defer></script>
       </Html>
     );
   }
